820dadb16bfd0d77679e9170975dc33659939b63
codexwarrior <96833259+codexwarrior@users.noreply.github.com>
2022-07-22 12:34:53 +0530

    Update App.js
    Marks of student

--- src/App.js
+++ src/App.js
@@ -5,18 +5,142 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <p></p>
+      <ul className="nav nav-pills">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            HOME
+          </a>
+        </li>
+        <li className="nav-item dropdown">
+          <button
+            className="nav-link dropdown-toggle"
+            data-bs-toggle="dropdown"
+            href="#"
+            role="button"
+            aria-expanded="false"
+          >
+            TEST
+          </button>
+          <ul className="test-menu">
+            <li>
+              <a className="dropdown-item" href="#">
+                PRACTICALS
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                QUIZES
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                CLASSTEST
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                HALIF YEARLY
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                FINAL RESULT
+              </a>
+            </li>
+            <div className="progress">
+              <div
+                className="progress-bar w-75"
+                role="progressbar"
+                aria-valuenow={75}
+                aria-valuemin={0}
+                aria-valuemax={100}
+              />
+            </div>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            RANK
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">PROGRESS CARD</a>
+        </li>
+      </ul>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item active" aria-current="page">
+            SCHOOL
+          </li>
+        </ol>
+      </nav>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <a href="#">SCHOOL</a>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            CLASS
+          </li>
+        </ol>
+      </nav>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <a href="#">SCHOOL</a>
+          </li>
+          <li className="breadcrumb-item">
+            <a href="#">CLASS</a>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            STUDENT
+          </li>
+        </ol>
+      </nav>
+      <ul className="list-group">
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard" viewBox="0 0 16 16">
+  <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z"/>
+  <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z"/>
+</svg>
+          MATHS
+          <span className="badge bg-primary rounded-pill">80</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard" viewBox="0 0 16 16">
+  <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z"/>
+  <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z"/>
+</svg>
+          SCIENCE
+          <span className="badge bg-primary rounded-pill">67</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard" viewBox="0 0 16 16">
+  <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z"/>
+  <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z"/>
+</svg>
+          ENGLISH
+          <span className="badge bg-primary rounded-pill">58</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard" viewBox="0 0 16 16">
+  <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z"/>
+  <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z"/>
+</svg>
+          SOCIAL SCIENCE
+          <span className="badge bg-primary rounded-pill">80</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard" viewBox="0 0 16 16">
+  <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z"/>
+  <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z"/>
+</svg>
+          HINDI
+          <span className="badge bg-primary rounded-pill">100</span>
+        </li>
+      </ul>
+      <p />
       </header>
     </div>
   );
